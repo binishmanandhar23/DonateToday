691c54acf9aac38df04982aaab447f9c130fd170
binishmanandhar23 <binish.manandhar23@gmail.com>
2023-08-05 23:26:55 +0545

    Setting duplicate email validation

--- app/src/main/java/com/sanket/donatetoday/repository/OnBoardingRepository.kt
+++ app/src/main/java/com/sanket/donatetoday/repository/OnBoardingRepository.kt
@@ -30,7 +30,7 @@ class OnBoardingRepository @Inject constructor(private val database: DatabaseRef
             (dataSnapshot.getValue<List<String>>()).let {
                 if(it == null){
                     database.child(FirebasePaths.Emails.node).setValue(listOf(user.emailAddress))
-                    setSignUpData(user = user)
+                    setSignUpData(user = user, onState = onState)
                 } else if(it.contains(user.emailAddress)) {
                     onState(LoginUIState.Error("Email already exists"))
                 } else{
@@ -40,24 +40,24 @@ class OnBoardingRepository @Inject constructor(private val database: DatabaseRef
                 }
             }
         }.addOnFailureListener {
-            setSignUpData(user = user)
+            setSignUpData(user = user, onState = onState)
         }
     }
 
-    private fun setSignUpData(user: User){
-        database.child(FirebasePaths.Emails.node).setValue(listOf(user.emailAddress))
-        database.child(FirebasePaths.Users.node).let {
-            when (user.userType) {
-                UserType.Donor.type -> it.child(FirebasePaths.Donors.node)
-                UserType.Organization.type -> it.child(FirebasePaths.Organizations.node)
-                else -> it
-            }
-        }.child(user.id).setValue(user)
-        /*auth.createUserWithEmailAndPassword(user.emailAddress, user.password).addOnCompleteListener { task ->
+    private fun setSignUpData(user: User, onState: (LoginUIState) -> Unit){
+        auth.createUserWithEmailAndPassword(user.emailAddress, user.password).addOnCompleteListener { task ->
             if (task.isSuccessful) {
+                database.child(FirebasePaths.Emails.node).setValue(listOf(user.emailAddress))
+                database.child(FirebasePaths.Users.node).let {
+                    when (user.userType) {
+                        UserType.Donor.type -> it.child(FirebasePaths.Donors.node)
+                        UserType.Organization.type -> it.child(FirebasePaths.Organizations.node)
+                        else -> it
+                    }
+                }.child(user.id).setValue(user)
                 onState(LoginUIState.Success("Successfully created user"))
             } else
                 onState(LoginUIState.Error(task.exception?.message))
-        }*/
+        }
     }
 }
