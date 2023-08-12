0aa5c6b3da11b1c92ebc50bfbd6b406c8596f2d5
binishmanandhar23 <binish.manandhar23@gmail.com>
2023-08-12 17:42:20 +0545

    Added one modified file name

--- app/src/main/java/com/sanket/donatetoday/models/dto/DonationItemOrganizationDTO.kt
+++
@@ -1,4 +0,0 @@
-package com.sanket.donatetoday.models.dto
-
-data class DonationItemUserModel(val id: String, val name: String)
-
---
+++ app/src/main/java/com/sanket/donatetoday/models/dto/DonationItemUserModel.kt
@@ -0,0 +1,4 @@
+package com.sanket.donatetoday.models.dto
+
+data class DonationItemUserModel(val id: String = "", val name: String = "")
+
